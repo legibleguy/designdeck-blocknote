d452609d8597eee0ba3e2778d99b384245bc340c
Andy <solovyovandy@yandex.ru>
2025-04-10 11:17:35 -0500

    toggles for list-card views

--- app/(editor)/_components/mechanicsNavigation.tsx
+++ app/(editor)/_components/mechanicsNavigation.tsx
@@ -1,14 +1,24 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { ChevronsLeft, MenuIcon, List, BookImage } from "lucide-react";
 import { ComponentRef, useRef, useState, useEffect } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
 import GameMechanicCardSmall from "./gameMechanicCardSmall";
 
-const MechanicsNav = () => {
-  const pathname = usePathname();
+interface MechanicsNavProps {
+  gameMechanics: {
+    id: string;
+    title: string;
+    category: string;
+    description: string;
+  }[];
+}
+
+const MIN_SIDEBAR_WIDTH = 309;
+
+const MechanicsNav: React.FC<MechanicsNavProps> = ({ gameMechanics }) => {
   const isMobile = useMediaQuery("(max-width: 768px)");
 
   const isResizingRef = useRef(false);
@@ -32,7 +42,7 @@ const MechanicsNav = () => {
     if (!isResizingRef.current) return;
     let newWidth = event.clientX;
 
-    if (newWidth < 276) newWidth = 276;
+    if (newWidth < MIN_SIDEBAR_WIDTH) newWidth = MIN_SIDEBAR_WIDTH;
     if (newWidth > 480) newWidth = 480;
 
     if (sidebarRef.current && navbarRef.current) {
@@ -62,9 +72,12 @@ const MechanicsNav = () => {
     } else {
       setIsCollapsed(false);
       if (sidebarRef.current && navbarRef.current) {
-        sidebarRef.current.style.width = "276px";
-        navbarRef.current.style.setProperty("left", "276px");
-        navbarRef.current.style.setProperty("width", "calc(100% - 276px)");
+        sidebarRef.current.style.width = `${MIN_SIDEBAR_WIDTH}px`;
+        navbarRef.current.style.setProperty("left", `${MIN_SIDEBAR_WIDTH}px`);
+        navbarRef.current.style.setProperty(
+          "width",
+          `calc(100% - ${MIN_SIDEBAR_WIDTH}px)`
+        );
       }
     }
   };
@@ -80,9 +93,12 @@ const MechanicsNav = () => {
     } else {
       setIsCollapsed(true);
       if (sidebarRef.current && navbarRef.current) {
-        sidebarRef.current.style.width = "0px";
-        navbarRef.current.style.setProperty("left", "0px");
-        navbarRef.current.style.setProperty("width", "100%");
+        sidebarRef.current.style.width = `${MIN_SIDEBAR_WIDTH}px`;
+        navbarRef.current.style.setProperty("left", `${MIN_SIDEBAR_WIDTH}px`);
+        navbarRef.current.style.setProperty(
+          "width",
+          `calc(100% - ${MIN_SIDEBAR_WIDTH}px)`
+        );
       }
     }
   };
@@ -99,9 +115,12 @@ const MechanicsNav = () => {
     } else {
       setIsCollapsed(false);
       if (sidebarRef.current && navbarRef.current) {
-        sidebarRef.current.style.width = "276px";
-        navbarRef.current.style.setProperty("left", "276px");
-        navbarRef.current.style.setProperty("width", "calc(100% - 276px)");
+        sidebarRef.current.style.width = `${MIN_SIDEBAR_WIDTH}px`;
+        navbarRef.current.style.setProperty("left", `${MIN_SIDEBAR_WIDTH}px`);
+        navbarRef.current.style.setProperty(
+          "width",
+          `calc(100% - ${MIN_SIDEBAR_WIDTH}px)`
+        );
       }
       document.body.style.overflowX = "auto";
     }
@@ -112,22 +131,42 @@ const MechanicsNav = () => {
       <aside
         ref={sidebarRef}
         className={cn(
-          "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
+          `group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]`,
           isResetting && "transition-all ease-in-out duration-300",
           isMobile && "w-0"
         )}
       >
-        {/* Example Game Mechanic Card */}
-        <GameMechanicCardSmall
-          title="Adaptive AI"
-          category="AI"
-          description="Dynamic adjustment of AI behaviour & difficulty."
-        />
+
+        <div className="flex justify-center mt-4">
+          <ToggleGroup type="single">
+          <ToggleGroupItem value="a">
+            <List role="button" className="h-4 w-4" />
+          </ToggleGroupItem>
+          <ToggleGroupItem value="b">
+            <BookImage role="button" className="h-4 w-4" />
+          </ToggleGroupItem>
+          </ToggleGroup>
+        </div>
+
+        <div className="flex flex-col gap-4 p-4 overflow-y-auto">
+          {gameMechanics.map((mechanic) => (
+            <GameMechanicCardSmall
+              key={mechanic.id}
+              title={mechanic.title}
+              category={mechanic.category}
+              description={mechanic.description}
+            />
+          ))}
+        </div>
+
+        <div>
+          Info Button will go here
+        </div>
 
         <div
           role="button"
           className={cn(
-            "h-6 w-6 cursor-pointer text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100",
+            `h-6 w-6 cursor-pointer text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100`,
             isMobile && "opacity-100"
           )}
         >
@@ -140,14 +179,14 @@ const MechanicsNav = () => {
         <div
           onMouseDown={handleMouseDown}
           onClick={() => {}}
-          className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
+          className={`opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0`}
         />
       </aside>
 
       <div
         ref={navbarRef}
         className={cn(
-          "absolute top-0 z-[99999] left-60 w-[calc(100%-276px)]",
+          `absolute top-0 z-[99999] left-${MIN_SIDEBAR_WIDTH}px w-[calc(100%-${MIN_SIDEBAR_WIDTH}px)]`,
           isResetting && "transition-all ease-in-out duration-300",
           isMobile && "left-0 w-full"
         )}
--- app/(editor)/layout.tsx
+++ app/(editor)/layout.tsx
@@ -3,15 +3,20 @@ import MechanicsNav from "./_components/mechanicsNavigation";
 import { supabase } from '@/app/utils/supabase';
 
 const RootLayout = async ({children} :  {children: React.ReactNode}) =>  {
-    
-    let { data: GameMechanic, error } = await supabase
+    let { data: gameMechanics, error } = await supabase
     .from('GameMechanic')
     .select('*');
-        
-    
+
+    gameMechanics = gameMechanics ?? [];
+
+    if (error) {
+        console.error("Error fetching game mechanics:", error);
+        gameMechanics = [];
+    }
+
     return ( 
         <div className="h-full flex">
-            <MechanicsNav />
+            <MechanicsNav gameMechanics={gameMechanics} />
             <main className="flex-1 h-full overflow-y-auto">
                 {children}
             </main>
